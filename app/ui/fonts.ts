46750fcd5ff1502dfd3fdf744f4c05be7e7e83f3
lunjz <lunjz@icloud.com>
2025-03-19 16:57:40 +0800

    add dashboard pages

---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+import { Inter } from 'next/font/google'; // 可能有网络问题
+import { Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({ weight: ['400', '700'], subsets: ['latin'] });
